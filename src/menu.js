90a53fd4bcfe7ff78bb832c678548f5ce6ab23dd
Christopher Hinstorff <chris@chinstorff.com>
2014-06-25 13:13:26 -0700

    add title to menuscreen

--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,8 @@ Game.Menu = function (game) { };
 
 Game.Menu.prototype = {
     create: function () {
-
+	A.background = game.add.sprite(-380, 0, 'background');
+	A.title = game.add.sprite(5, 5, 'title');
     },
 
     update: function () {
