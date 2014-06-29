cfd7fbe01683ec23093881def772aeae1eba1b1d
Christopher Hinstorff <chris@chinstorff.com>
2014-06-28 21:52:36 -0700

    increase music volume

--- src/menu.js
+++ src/menu.js
@@ -13,28 +13,29 @@ Game.Menu.prototype = {
 
     update: function () {
 	if (A.timer && game.time.now - A.timer > 1750) {
+	    A.timer = 0;
 	    game.state.start('Play');
 	}
     },
 
     shiftTween: function (object) {
-	if (!A.timer) {
-	    game.add.tween(object)
-		.to({ x: object.x -205 }, 300, null, true, 0, 0, false)
-		.to({ frame: 1 }, 1, null, true, 0, 0, false)
-		.to({ x: object.x + 369 }, 600, null, true, 200, 0, false)
-		.to({ frame: 2 }, 1, null, true, 0, 0, false)
-		.to({ x: object.x + 82 }, 400, null, true, 200, 0, false);
-	}
+	game.add.tween(object)
+	    .to({ x: object.x -205 }, 300, null, true, 0, 0, false)
+	    .to({ frame: 1 }, 1, null, true, 0, 0, false)
+	    .to({ x: object.x + 369 }, 600, null, true, 200, 0, false)
+	    .to({ frame: 2 }, 1, null, true, 0, 0, false)
+	    .to({ x: object.x + 82 }, 400, null, true, 200, 0, false);
     },
 
     endMenu: function () {
-	this.shiftTween(A.title);
-	this.shiftTween(A.background);
-	A.timer = A.timer || game.time.now;
-
+	if (!A.timer) {
+	    this.shiftTween(A.title);
+	    this.shiftTween(A.background);
+	    A.timer = game.time.now;
+	}
+	    
 	if (A.audio) {
-	    A.music.play('', 0, 0.5, true, false);
+	    A.music.play('', 0, 0.7, true, true);
 	}
     },
 };
